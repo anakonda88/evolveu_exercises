b172e7d6afaf94402053ebe90a9f3ef709d95e22
Ana <anakonda.88@gmail.com>
2018-11-05 14:13:13 -0700

    JS tutorial exercises

--- JS/Script.js
+++ JS/Script.js
@@ -1,4 +1,4 @@
-/*var firstName = 'Ana';
+/*var firstName = 'ariel';
 console.log(firstName);
 
 var lastName = 'Martinez';
@@ -17,47 +17,47 @@ console.log(firstName + " " + lastName);
 /*******************************************
 * Basic Operators
 */
-var year, yearAna, yearElias;
+var year, yearAriel, yearRonald;
 now = 2018;
-ageAna = 41;
-ageElias = 48;
+ageAriel = 41;
+ageRonald = 48;
 
 //Math Operators
 
-yearAna = now - 41;
-yearElias = now - 48;
+yearAriel = now - 41;
+yearRonald = now - 48;
 
-console.log(yearAna);
+console.log(yearAriel);
 
 console.log(now * 3);
 console.log(now + 45);
 console.log(now / 10);
 
 //Logical Operators
-var anaOlder = ageAna < ageElias
-console.log(anaOlder);
+var arielOlder = ageAriel < ageRonald
+console.log(arielOlder);
 
 // type of Operators
-console.log(typeof anaOlder);
-console.log(typeof ageAna);
-console.log(typeof 'Elias is older than Ana');
+console.log(typeof arielOlder);
+console.log(typeof ageAriel);
+console.log(typeof 'ronald is older than ariel');
 
 var x;
 console.log(typeof x);
 
 //Operator precedence
 var now = 2018;
-var yearAna = 1977;
+var yearAriel = 1977;
 var fullAge = 41;
 
 //Multiple operators
-var isFullAge = now - yearAna >= fullAge;
+var isFullAge = now - yearAriel >= fullAge;
 console.log(isFullAge);
 
 //Grouping
-var ageAna = now - yearAna;
-var ageElias = now - yearElias;
-var average = (ageAna + ageElias) / 2;
+var ageAriel = now - yearAriel;
+var ageRonald = now - yearRonald;
+var average = (ageAriel + ageRonald) / 2;
 console.log(average);
 
 //Multiple Operators
@@ -82,16 +82,16 @@ heighAK = 159;
 var akBMI = massAK / (heighAK * heighAK);
 console.log(akBMI);
 
-massElias = 86;
-heighElias = 182;
-var eliasBMI = massElias / (heighElias * heighElias);
-console.log(eliasBMI);
+massRonald = 86;
+heighRonald = 182;
+var ronaldBMI = massRonald / (heighRonald * heighRonald);
+console.log(ronaldBMI);
 
-var bmiComparisson = eliasBMI <= akBMI;
-console.log("Is Ana's BMI higher than Elias's? "+bmiComparisson);
+var bmiComparisson = ronaldBMI <= akBMI;
+console.log("Is ariel's BMI higher than ronald's? "+bmiComparisson);
 
 //if-else statements
-var firstName = 'Ana';
+var firstName = 'ariel';
 var civilStatus = 'married';
 
 var isMarried = true;
@@ -108,20 +108,20 @@ heighAK = 159;
 var akBMI = massAK / (heighAK * heighAK);
 console.log(akBMI);
 
-massElias = 86;
-heighElias = 182;
-var eliasBMI = massElias / (heighElias * heighElias);
-console.log(eliasBMI);
+massRonald = 86;
+heighRonald = 182;
+var ronaldBMI = massRonald / (heighRonald * heighRonald);
+console.log(ronaldBMI);
 
-if (eliasBMI > akBMI){
-  console.log('Elias\'s BMI is higher than Ana\'s');
+if (ronaldBMI > akBMI){
+  console.log('ronald\'s BMI is higher than ariel\'s');
 }else {
-  console.log('Ana\'s BMI is higher than Elias\'s');
+  console.log('ariel\'s BMI is higher than ronald\'s');
 }
 
 //Boolean Logic
 
-var firstName = 'Oriana';
+var firstName = 'Ori';
 var age = 37;
 
 if (age < 13){
@@ -163,7 +163,7 @@ switch(job){
     console.log(firstName + ' does something else');
 }
 
-var firstName = 'Mariana';
+var firstName = 'Athena';
 var age = 37;
 switch(true){
   case age < 13:
@@ -218,10 +218,10 @@ else if (maryTeamAvg > jTeamAvg && maryTeamAvg > mTeamAvg){
 function calculateAge(birthYear) {
   return 2018 - birthYear;
 }
-var ageAna = calculateAge(1977);
-var ageElias = calculateAge(1950);
+var ageAriel = calculateAge(1977);
+var ageRonald = calculateAge(1950);
 var ageOri = calculateAge(1996);
-console.log(ageAna, ageElias, ageOri);
+console.log(ageAriel, ageRonald, ageOri);
 
 function yearsUntilretirement(year, firstName) {
   var age = calculateAge(year);
@@ -233,8 +233,8 @@ function yearsUntilretirement(year, firstName) {
     console.log(firstName + ' is already retired');
   }
 }
-yearsUntilretirement(1977,'Ana');
-yearsUntilretirement(1950, 'Elias');
+yearsUntilretirement(1977,'ariel');
+yearsUntilretirement(1950, 'ronald');
 yearsUntilretirement(1996, 'Ori');
 
 //Challenge #3
@@ -261,3 +261,170 @@ var finalPayment = [bill[0] + tips[0],
 console.log(tips, finalPayment);
 
 //Challenge #4
+var aK = {
+  fullName: "Ariel Garcia",
+  mass: 64,
+  heigh: 1.59,
+  calculateBMI: function() {
+    this.bmi = this.mass / (this.heigh * this.heigh);
+    return this.bmi;
+  }
+}
+
+var ronald = {
+  fullName: 'Ronald McDonald',
+  mass: 86,
+  heigh: 1.82,
+  calculateBMI: function() {
+    this.bmi = this.mass / (this.heigh * this.heigh);
+    return this.bmi;
+  }
+}
+aK.calculateBMI();
+ronald.calculateBMI();
+console.log(aK, ronald);
+
+if (aK.calculateBMI() > ronald.calculateBMI()) {
+  console.log(aK.fullName + ' has a BMI of ' + aK.calculateBMI() +
+   ' which is higher than ' + ronald.fullName + ' BMI');
+
+} else if (ronald.bmi > aK.bmi){
+  console.log(ronald.fullName + ' has a BMI of ' + ronald.calculateBMI() +
+   ' which is higher than ' + aK.fullName + ' BMI');
+
+} else {
+  console.log('They have the same BMI');
+}
+
+//Loops and iteration
+for (var i = 0; i < 20; i +=2){
+  console.log(i);
+}
+
+var mena = ['Minerva', 'McDonald', 2002, 'student', 'daughter'];
+for (var i = 0; i < mena.length; i++) {
+  console.log(mena[i]);
+}
+
+//while Loop
+
+var i = 0;
+while (i < mena.length) {
+  console.log(mena[i]);
+  i+=2;
+}
+
+//continue and break statements
+var mena = ['Minerva', 'McDonald', 2002, 'student', 'daughter'];
+for (var i = 0; i < mena.length; i++) {
+  if (typeof mena[i] !== 'string') continue;
+  console.log(mena[i]);
+}
+
+for (var i = 0; i < mena.length; i++) {
+  if (typeof mena[i] !== 'string') break;
+  console.log(mena[i]);
+}
+
+//looping backwards
+
+for (var i = mena.length - 1; i >= 0; i--) {
+  console.log(mena[i]);
+}
+
+//challenge # 5
+/*function tipCalculator(bill){
+  var percentage;
+  if (bill < 50){
+    return bill * 20 / 100;
+  }else if  (bill >= 50 && 200){
+    return bill * 15 / 100;
+  }else {
+    return bill * 10 / 100;
+  }
+}
+
+var bill = [124, 48, 268];
+var tips = [tipCalculator(bill[0]),
+            tipCalculator(bill[1]),
+            tipCalculator(bill[2])]
+
+var finalPayment = [bill[0] + tips[0],
+                    bill[1] + tips[1],
+                    bill[2] + tips[2]]
+console.log(tips, finalPayment);*/
+
+var ronald = {
+  fullName: 'Ronald McDonald',
+  bills: [1124, 548, 268, 180, 42],
+  tipCalc: function() {
+    this.tips = [];
+    this.finalValues = [];
+
+    for (var i = 0; i < this.bills.length; i++)
+    {
+      var percentagE;
+      var bill = this.bills[i];
+
+      if (bill < 50){
+        percentagE = .2;
+      } else if  (bill >= 50 && bill < 200){
+        percentagE = .15;
+      } else {
+        percentagE = .1;
+      }
+      this.tips[i] = bill * percentagE;
+      this.finalValues[i] = bill + bill * percentagE;
+    }
+  }
+}
+
+
+
+var ariel = {
+  fullName: 'Ariel Garcia',
+  bills: [77, 375, 110, 45],
+  tipCalc: function() {
+    this.tips = [];
+    this.finalValues = [];
+
+    for (var i = 0; i < this.bills.length; i++)
+    {
+      var percentagE;
+      var bill = this.bills[i];
+
+      if (bill < 100){
+        percentagE = .2;
+      } else if  (bill >= 100 && bill < 300){
+        percentagE = .1;
+      } else {
+        percentagE = .25;
+      }
+      this.tips[i] = bill * percentagE;
+      this.finalValues[i] = bill + bill * percentagE;
+    }
+  }
+}
+
+function calcAveg(tips) {
+  var sum = 0;
+  for (var i = 0; i < tips.length; i++) {
+    sum = sum + tips[i];
+  }
+  return sum / tips.length;
+}
+ronald.tipCalc();
+ariel.tipCalc();
+
+ronald.average = calcAveg(ronald.tips);
+ariel.average = calcAveg(ariel.tips);
+
+console.log(ronald, ariel);
+
+if (ronald.average > ariel.average){
+  console.log(ronald.fullName +'\'s family pays higher tips with an avg of $ ' +
+              ronald.average );
+} else if (ariel.average > ronald.average) {
+  console.log(ariel.fullName +'\'s family pays higher tips with an avg of $ ' +
+              ariel.average );
+}
