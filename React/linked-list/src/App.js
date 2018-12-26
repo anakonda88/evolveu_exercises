af54b6a16679afd7643da8ca61ce213f9cf3244f
Ana <anakonda.88@gmail.com>
2018-12-26 12:23:56 -0700

    updating App

--- React/linked-list/src/App.js
+++ React/linked-list/src/App.js
@@ -1,36 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import LinkedListComp from './components/LinkedListComp.js';
 import MyMathComp from './components/MyMathComp.js';
 import CommunityComp from './components/CommunityComp.js';
+import PeopleComp from './components/PeopleComp.js';
+
 
 class App extends Component {
-  onPassedFunction = ()=>{
-    console.log('We are in the app function');
-  }
+  // onPassedFunction = ()=>{
+  //   console.log('We are in the app function');
+  // }
   render() {
     return (
       <div className="App">
-      <h1>Hello World 1</h1>
-      <MyMathComp name='Top1'/>
-      <CommunityComp name='Top2'/>
-      <LinkedListComp name='Top3' func={this.onPassedFunction}/>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Underpuppies App</h1>
+        <MyMathComp name='Top1'/>
+        <CommunityComp name='Top2'/>
+        <PeopleComp name='Top3'/>
       </div>
+
+
+
     );
   }
 }
